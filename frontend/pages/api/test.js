9086f0fb983dc3d0e006729ece1b1abe44b3efa1
Andrew <anbusiness04@gmail.com>
2025-02-11 13:06:28 -0600

    API communication fix

--- frontend/pages/api/test.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export default async function handler(req, res) {
-  const response = await axios.get('http://127.0.0.1:5000/api/hello');
-  res.status(200).json(response.data);
-}
